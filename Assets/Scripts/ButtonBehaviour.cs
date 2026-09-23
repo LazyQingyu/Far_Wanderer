952db6af0991a59e82b6cb5c6d66227c1ffeaa91
LazyQingyu <lazyqingyu@proton.me>
2026-09-23 15:36:54 +0400

    add action on click for button start and button score board

--- Assets/Scripts/ButtonBehaviour.cs
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonBehaviour : MonoBehaviour
 {
     public void GoToGame()
     {
-
+        SceneManager.LoadSceneAsync("Game");
     }
 
     public void GoToScoreBoard()
     {
-
+        SceneManager.LoadSceneAsync("ScoreBoard");
     }
 
 }
