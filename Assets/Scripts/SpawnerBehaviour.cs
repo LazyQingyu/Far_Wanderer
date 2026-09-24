85d814ee40ff250ba31b1218666cadc4ef1f73c8
LazyQingyu <lazyqingyu@proton.me>
2026-09-24 15:50:42 +0400

    Check is horizontal move is possible

--- Assets/Scripts/SpawnerBehaviour.cs
+++ Assets/Scripts/SpawnerBehaviour.cs
@@ -1,8 +1,9 @@
 using NUnit.Framework;
-using UnityEngine;
-using Unity.Collections;
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SpawnerBehaviour : MonoBehaviour
 {
@@ -34,7 +35,7 @@ public class SpawnerBehaviour : MonoBehaviour
 
     private void FixedUpdate()
     {
-        moveHorizontaly(listAlien, -8.5f, -1);
+        moveHorizontaly(listAlien, -1);
     }
     public void InstantiateAliensGroup(int startX, int startY, int[,] enemyMap )
     {
@@ -83,17 +84,22 @@ public class SpawnerBehaviour : MonoBehaviour
     }
 
 
-    void moveHorizontaly(List<GameObject> aliansWhoIsAlive, float maxPossibleMove, float moveBy)
+    void moveHorizontaly(List<GameObject> aliansWhoIsAlive, float moveBy)
     {
         foreach(GameObject alien in aliansWhoIsAlive)
         {
             float currentAlienPosX = alien.transform.position.x;
             float currentAlienPosY = alien.transform.position.y;
-            if(currentAlienPosX + moveBy > maxPossibleMove)
+            if(CanMoveHorizontaly( currentAlienPosX + moveBy) && CanMove(alien, aliansWhoIsAlive, ("Horizontal",moveBy)))
             {
                 alien.transform.position = new Vector3(currentAlienPosX + moveBy, currentAlienPosY, 0);
             }
         }
+    }
+
+    bool CanMoveHorizontaly(float pos)
+    {
+        return (-8.5f < pos && pos < 8.5f);
     }
 
     void moveVerticaly(List<GameObject> aliansWhoIsAlive, float maxPossibleMove, float moveBy)
@@ -102,22 +108,24 @@ public class SpawnerBehaviour : MonoBehaviour
         {
             float currentAlienPosX = alien.transform.position.x;
             float currentAlienPosY = alien.transform.position.y;
-            if (currentAlienPosY + moveBy < maxPossibleMove)
+            if (currentAlienPosY + moveBy < maxPossibleMove && CanMove(alien, aliansWhoIsAlive, ("Verticaly", moveBy)))
             {
                 alien.transform.position = new Vector3(currentAlienPosX, currentAlienPosY + moveBy, 0);
             }
         }
     }
 
-    bool CanMove(GameObject alien, List<GameObject> alienSwam, (string,float) Move)
+    bool CanMove(GameObject alien, List<GameObject> alienSwam, (string,float) move)
     {
 
+        (float, float) alienNextPos = getNextPos(alien, move);
         foreach( GameObject oneAlienInSwamp in alienSwam)
         {
-
             if (!IsMySelf(alien, oneAlienInSwamp))
             {
-
+                if (HaveSamePos(alienNextPos, oneAlienInSwamp)){
+                    return false;
+                }
             }
         }
         return true;
@@ -128,6 +136,27 @@ public class SpawnerBehaviour : MonoBehaviour
         return (alien.transform.position.x == oneAlienInSwam.transform.position.x && alien.transform.position.y == oneAlienInSwam.transform.position.y);
     }
 
+    bool HaveSamePos((float, float) posAlienAfterMove, GameObject oneAlienInSwam)
+    {
+        return (posAlienAfterMove.Item1 == oneAlienInSwam.transform.position.x && posAlienAfterMove.Item2 == oneAlienInSwam.transform.position.y);
+    }
+
+    (float, float) getNextPos(GameObject alien,(string, float) move)
+    {
+        (float, float) nextPos;
+        if (move.Item1 == "Horizontal")
+        {
+            nextPos.Item1 = alien.transform.position.x + move.Item2;
+            nextPos.Item2 = alien.transform.position.y;
+        }
+        else
+        {
+            nextPos.Item1 = alien.transform.position.x;
+            nextPos.Item2 = alien.transform.position.y + move.Item2;
+        }
+
+        return nextPos;
+    }
 
 
 
